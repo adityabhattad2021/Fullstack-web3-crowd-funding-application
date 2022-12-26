5e03d357b6e4373aa99768801e0ecfb4631c9765
Aditya Bhattad <adityabhattad18@gmail.com>
2022-12-26 09:28:41 +0530

    Smart Contract v0.1 complete

--- crowdfunding/hardhat.config.js
+++ crowdfunding/hardhat.config.js
@@ -1,12 +1,45 @@
+require("dotenv").config();
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: {
-    version: '0.8.9',
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
-    },
-  },
+	solidity: {
+		version: "0.8.17",
+		settings: {
+			optimizer: {
+				enabled: true,
+				runs: 200,
+			},
+		},
+	},
+	defaultNetwork: "goerli",
+	networks: {
+		hardhat: {
+			chainId: 1337,
+			blockConfirmations: 1,
+		},
+		localhost: {
+			chainId: 1337,
+			blockConfirmations: 1,
+		},
+		goerli: {
+			chainId: 5,
+			blockConfirmations: 6,
+			url: GOERLI_RPC_URL,
+			accounts: [PRIVATE_KEY],
+		},
+	},
+	namedAccounts: {
+		deployer: {
+			default: 0,
+		},
+		user1: {
+			default: 1,
+		},
+		user2: {
+			default: 2,
+		},
+	},
 };
