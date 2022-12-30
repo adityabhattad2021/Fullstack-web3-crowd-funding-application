c701e7a06c3dedfe0c2b6d533841a14597b237a8
Aditya Bhattad <adityabhattad18@gmail.com>
2022-12-30 17:12:00 +0530

    Profile Page v0.0.1 ✅

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,10 +1,39 @@
-function Profile(){
-    return (
-        <div>
-            Profile
-        </div>
-    )
-}
+import { useContract } from "@thirdweb-dev/react";
+import { useState, useEffect } from "react";
+import { DisplayCampaigns } from "../components";
+import { useStateContext } from "../context";
 
+function Profile() {
+	const [isLoading, setIsLoading] = useState(false);
+	const [campaigns, setCampaigns] = useState([]);
+
+	const { address, getCampaigns } = useStateContext();
+	const { contract } = useContract(
+		"0x7f569Bd5fF6b3a5d261630CEB9623c976f5d0076"
+	);
+
+	async function fetchCampaigns() {
+		setIsLoading(true);
+		const data = await getCampaigns();
+		setCampaigns(data);
+		setIsLoading(false);
+	}
+
+    useEffect(()=>{
+        if(contract){
+            fetchCampaigns()
+        }else{
+            console.log("contract is not connected");
+        }
+    },[address,contract])
+
+	return (
+        <DisplayCampaigns
+            title="Campaigns created by you"
+            isLoading={isLoading}
+            campaigns={campaigns}
+        />
+    );
+}
 
 export default Profile;
