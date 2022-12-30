19f4b3024881c31a60762ca95f2bc7df1f6d5518
Aditya Bhattad <adityabhattad18@gmail.com>
2022-12-30 17:09:58 +0530

    Home Page v0.0.1 ✅

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { useContract } from "@thirdweb-dev/react";
 import { useState, useEffect } from "react";
 import { DisplayCampaigns } from "../components";
 import { useStateContext } from "../context";
@@ -6,7 +7,10 @@ function Home() {
 	const [isLoading, setIsLoading] = useState(false);
 	const [campaigns, setCampaigns] = useState([]);
 
-	const { address, contract, getCampaigns } = useStateContext();
+	const { address, getCampaigns } = useStateContext();
+	const { contract } = useContract(
+		"0x7f569Bd5fF6b3a5d261630CEB9623c976f5d0076"
+	);
 
 	async function fetchCampaigns() {
 		setIsLoading(true);
@@ -15,23 +19,21 @@ function Home() {
 		setIsLoading(false);
 	}
 
-    useEffect(()=>{
-        if(contract){
-            fetchCampaigns()
-        }else{
-            console.log("contract not connected.");
-        }
-    },[])
-
- 
+	useEffect(() => {
+		if (contract) {
+			fetchCampaigns();
+		} else {
+			console.log("contract not connected.");
+		}
+	}, [address,contract]);
 
 	return (
-        <DisplayCampaigns
-            title="All Campaigns"
-            isLoading={isLoading}
-            campaigns={campaigns}
-        />
-    );
+		<DisplayCampaigns
+			title="All Campaigns"
+			isLoading={isLoading}
+			campaigns={campaigns}
+		/>
+	);
 }
 
 export default Home;
