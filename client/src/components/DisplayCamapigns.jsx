5cae7341b1669fb3301be7550763240965a0a76e
Aditya Bhattad <adityabhattad18@gmail.com>
2022-12-30 17:11:19 +0530

    Modularized Reusable Components

--- client/src/components/DisplayCamapigns.jsx
+++ client/src/components/DisplayCamapigns.jsx
@@ -1,16 +1,46 @@
 import { useNavigate } from "react-router-dom";
 
 import { loader } from "../assets";
+import { FundCard } from "../components";
 
-function DisplayCampaigns({title,isLoading,campaigns}) {
+function DisplayCampaigns({ title, isLoading, campaigns }) {
+	const navigate = useNavigate();
 
-    const navigate=useNavigate()
+	function handleNavigate(campaign) {
+		navigate(`/campaign-details/${campaign.title}`, { state: campaign });
+	}
 
 	return (
-        <div className="flex justify-center">
-            <h1 className="font-epilogue font-semibold text-[18px] text-white text-left">{title} ({campaigns.length})</h1>
-        </div>
-    );
+		<div className="">
+			<h1 className="font-epilogue font-semibold text-[18px] text-white text-left">
+				{title} ({campaigns.length})
+			</h1>
+
+			<div className="flex flex-wrap mt-[20px] gap-[26px]">
+				{isLoading && (
+					<img
+						src={loader}
+						alt="Loader"
+						className="w-[100px] h-[100px] object-contain"
+					/>
+				)}
+				{!isLoading && campaigns.length === 0 && (
+					<p className="font-epilogue font-semibold text-[14px] leading-[30px] text-[#818183] ">
+						No campaign found
+					</p>
+				)}
+				{!isLoading &&
+					campaigns.length > 0 &&
+					campaigns.map((campaign) => (
+						<FundCard
+							key={campaign.camapignId}
+							{...campaign}
+							handleClick={() => handleNavigate(campaign)}
+						/>
+					))}
+			</div>
+		</div>
+	);
 }
 
 export default DisplayCampaigns;
