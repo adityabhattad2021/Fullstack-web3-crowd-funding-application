7c456ba965126ae62c5ec734e9416059f61bc42d
Aditya Bhattad <adityabhattad18@gmail.com>
2022-12-30 19:37:51 +0530

    Update README.md

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -2,7 +2,7 @@ import { loader } from "../assets";
 
 function Loader() {
 	return (
-		<div className="fixed inset-0 z-10 h-screen bg-[rgba(0,0,0,0.7)] flex items-center justify-center">
+		<div className="fixed inset-0 z-10 h-screen bg-[rgba(0,0,0,0.7)] flex items-center justify-center flex-col">
 			<img
 				src={loader}
 				alt="loader"
